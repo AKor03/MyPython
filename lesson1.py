4b6d3dada80e0c5fa29d326ebe4d75d883d35463
Eugene <jenecka0@gmail.com>
2017-10-02 16:52:54 +0300

    v3

--- lesson1.py
+++ lesson1.py
@@ -5,5 +5,11 @@ print('trunc(x) =', math.trunc(x))
 print('floour(x) =', math.floor(x))
 print('ceil(x) =', math.ceil(x))
 
+# числа pi, e
 print(math.pi)
 print(math.e)
+
+#sin pi/4
+print(math.sin(math.pi / 4))
+# 1 делим на корень из 2х
+print(1 /math.sqrt(2))
