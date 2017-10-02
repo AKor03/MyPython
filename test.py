3256b977166f928426df5e7e9dfb9df557630239
Eugene <jenecka0@gmail.com>
2017-10-02 12:19:08 +0300

    v1

---
+++ test.py
@@ -0,0 +1,17 @@
+x = 5
+y = 6
+
+print(x + y)
+print(x + 3)
+print(x - y)
+
+print()
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print()
+
+print(x ** y)
+print(0.5 ** y)
+print(pow(x, y))
