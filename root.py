9168b741c1a058efab229494d1c0fc7a7d0de9dc
Eugene <jenecka0@gmail.com>
2017-10-03 14:47:40 +0300

    v5

--- root.py
+++ root.py
@@ -1,10 +1,17 @@
 import math
 a = 2
-b = 3
+b = 10
 c = 4
-d = ((b ** 2) - (4 * a * c))
 
-#print(-b + math.sqrt(d)) / (2 * a))
-#print(-b + math.sqrt(d))
-#print(math.sqrt(d))
-print(d)
+
+d = ((b ** 2) - (4 * a * c))
+#print(d)
+#print((-b + math.sqrt(d)) / (2 * a))
+x1 = (-b + math.sqrt(d)) / (2 * a)
+x2 = (-b - math.sqrt(d)) / (2 * a)
+
+print(x1)
+
+print(x2)
+print("x1 = {}".format(x1))
+print("x2 = {}".format(x2))
