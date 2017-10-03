9c8ac199c58af042ba5acdaa75c33162776cfda2
Eugene <jenecka0@gmail.com>
2017-10-03 15:46:29 +0300

    v6

--- root.py
+++ root.py
@@ -1,17 +1,20 @@
-import math
-a = 2
-b = 10
-c = 4
+print("Введи значение a")
+a = float(input("a = "))
+print("Введи значение b")
+b = float(input("b = "))
+print("Введи значение c")
+c = float(input("c = "))
 
-
-d = ((b ** 2) - (4 * a * c))
-#print(d)
-#print((-b + math.sqrt(d)) / (2 * a))
-x1 = (-b + math.sqrt(d)) / (2 * a)
-x2 = (-b - math.sqrt(d)) / (2 * a)
-
-print(x1)
-
-print(x2)
-print("x1 = {}".format(x1))
-print("x2 = {}".format(x2))
+d = b ** 2 - 4 * a * c;
+print("d = {}".format(d))
+if d > 0:
+    import math
+    x1 = (-b + math.sqrt(d)) / (2 * a)
+    x2 = (-b - math.sqrt(d)) / (2 * a)
+    print("x1 = {}".format(x1))
+    print("x2 = {}".format(x2))
+elif d == 0:
+    x = -b / (2 * a)
+    print("x = {}".format(x))
+else:
+    print("нет корня")
